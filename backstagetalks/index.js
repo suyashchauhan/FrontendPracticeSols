52657e568d32884095ecbaf7292205d399b912ca
suyashchauhan <suyashchauhan31@gmail.com>
2021-07-16 00:01:45 +0530

    Backstage Complete

--- backstagetalks/index.js
+++ backstagetalks/index.js
@@ -2,38 +2,64 @@ const sections = document.querySelectorAll('section')
 const links = document.querySelectorAll('ul li a')
 const container = document.querySelector('.container')
 const height = sections[0].offsetHeight;
+let currentSlide = 0;
 
-let currentSlide = 1;
-links.forEach((link) => {
-    link.addEventListener('click', (e) => {
-        currentSlide = link.getAttribute('href').substr(6, 1) - 1
-        updateLinkHighlight()
-    })
-})
+
 let array = ["00c1b5", "ff6519", "ffbe00", "1d3fbb", "e30512"];
 let previousTime = new Date().getTime();
-document.addEventListener('wheel', function (e) {
+
+ScreenSize()
+window.addEventListener('resize', function (e) {
+    let currentTime = new Date().getTime();
+    let difference = currentTime - previousTime;
+    if (difference > 1000) {
+        ScreenSize()
+    }
+    previousTime=currentTime;
+})
+
+function ScreenSize() {
+    if (window.innerWidth > 996) {
+        window.removeEventListener('scroll', handleScroll);
+        document.addEventListener('wheel', handleWheel)
+        links.forEach((link) => {
+            link.addEventListener('click', handleLink)
+        })
+    }
+    else {
+        document.removeEventListener('wheel', handleWheel)
+        window.addEventListener('scroll', handleScroll);
+        links.forEach((link) => {
+            link.removeEventListener('click', handleLink)
+        })
+    }
+}
+function handleWheel(e) {
     let currentTime = new Date().getTime();
     let difference = currentTime - previousTime;
     if (difference > 500) {
         if (e.deltaY > 0) {
             if (currentSlide < sections.length - 1) currentSlide++;
-            container.scrollTo(0, height * currentSlide)
-            container.style.background = `#${array[currentSlide]}`;
-            updateLinkHighlight();
         }
         else {
             if (currentSlide > 0)
                 currentSlide--;
-            container.scrollTo(0, height * currentSlide)
-            container.style.background = `#${array[currentSlide]}`;
-            updateLinkHighlight();
-
         }
-
+        container.scrollTo(0, height * currentSlide)
+        container.style.background = `#${array[currentSlide]}`;
+        updateLinkHighlight();
         previousTime = currentTime;
     }
-})
+}
+function handleLink() {
+    currentSlide = link.getAttribute('href').substr(6, 1) - 1
+    container.style.background = `#${array[currentSlide]}`;
+    updateLinkHighlight()
+}
+function handleScroll() {
+    container.style.background = `#${array[Math.floor(window.scrollY / window.innerHeight)]}`;
+}
+
 function updateLinkHighlight() {
 
     links.forEach((a) => {
